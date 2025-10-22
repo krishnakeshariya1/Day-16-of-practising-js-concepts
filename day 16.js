7cbea8e5083cd70aa15d4ea8a0d20397cc1cd082
krishna keshariya <keshariyakrishna*@gmail.com>
2025-10-22 13:26:27 +0530

    added question like array of object

--- day 16.js
+++ day 16.js
@@ -11,3 +11,24 @@ console.log(word);
 let numbers = [1,2,3,4,5,6,7];
 let firstEvenNumber = numbers.find(val => val%2 === 0);
 console.log(`the fist even number is :- ${firstEvenNumber}`); 
+
+// find the first object where age is grater than 20
+const user = [
+    {name: "kashish" , age: 20 },
+    {name: "kirti" , age: 19 },
+    {name: "raj" , age: 21 },
+    {name: "rahul" , age: 22 },
+];
+let age = user.find(val => val.age > 20);
+console.log(`The person who has age greater than 20 is : ${JSON.stringify(age)}`);
+
+// find the first number that is divisble by 2 and 3 both, i'm using the the above array (number).
+let ans = numbers.find(val => val%2 === 0 && val%3 === 0);
+console.log(ans);
+
+// find the index of the first word containing "a" in the following array 
+let type = ["sky", "flower","rain","forest"];
+let wordWithA = type.find(val => val.includes("a"));
+console.log(`The word that contain a is :- "${wordWithA}"`);
+
+// 
